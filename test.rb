26b667199085ffe2ec478762681167b44d89f5ff
Felipe Contreras <felipe.contreras@gmail.com>
2008-09-07 16:22:07 +0300

    Cleanups.

--- test.rb
+++ test.rb
@@ -1,15 +1,15 @@
 require 'mtag'
 
 def test_file(fn)
-	file = MTag::File.new(fn)
-        file.strip("id3v1")
-        p file.tag.get_all
-	print("%s - %s\n" % [file.tag.artist, file.tag.title])
+  file = MTag::File.new(fn)
+  file.strip("id3v1")
+  p file.tag.get_all
+  print("%s - %s\n" % [file.tag.artist, file.tag.title])
   file.tag.set("artist", "foo")
-	print("%s - %s\n" % [file.tag.get("artist"), file.tag.get("title")])
-	print("%s - %s\n" % [file.tag.artist, file.tag.title])
-	# file.tag.title = "foo";
-	# file.save();
+  print("%s - %s\n" % [file.tag.get("artist"), file.tag.get("title")])
+  print("%s - %s\n" % [file.tag.artist, file.tag.title])
+  # file.tag.title = "foo";
+  # file.save();
 end
 
 test_file(ARGV[0])
